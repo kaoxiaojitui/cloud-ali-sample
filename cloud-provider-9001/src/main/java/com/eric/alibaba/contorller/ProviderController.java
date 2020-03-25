b780a903e0765316f301ffdc286b96e6c0d0c77c
kaoxiaojitui <kaoxiaojitui@github.com>
2020-03-25 20:10:54 +0800

    get config from nacos-config-center

--- cloud-provider-9001/src/main/java/com/eric/alibaba/contorller/ProviderController.java
+++ cloud-provider-9001/src/main/java/com/eric/alibaba/contorller/ProviderController.java
@@ -1,6 +1,8 @@
 package com.eric.alibaba.contorller;
 
+import com.alibaba.nacos.api.config.annotation.NacosValue;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2020/3/24
  */
 @RestController
+@RefreshScope
 public class ProviderController {
     @Value("${server.port}")
     private String port;
@@ -16,5 +19,12 @@ public class ProviderController {
     @GetMapping(value = "/provider")
     public String getProvider(){
         return port;
+    }
+
+    @Value("${config.addr}")
+    private String addr;
+    @GetMapping(value = "/config")
+    public String getConfig(){
+        return addr;
     }
 }
